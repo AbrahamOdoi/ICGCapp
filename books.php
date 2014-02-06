cd9bb3aefc78ac11e474e50ba3f5e16f2447c7b2
AbrahamOdoi <abrahamodoi2002@gmail.com>
2014-02-06 11:04:10 +0000

    5.7
    5.7

--- books.php
+++ books.php
@@ -70,6 +70,8 @@
 					Price: $6.00
 					<br/>
 					With no clear sense of where we are going, we are bound to settle for any destination. Without vision, we easily accept anything that comes our way and allow other people to set the agenda for our lives. That is why we ned to set goals and go on to achieve them. Setting goals requires that you learn from your past experiences, locate your current opportunities and launch boldly into your future.
+					<br />
+					Comment prendre des décisions éclairées présente comme approche systématique et pourtant simple qui comporte six étapes clés à recevoir de bons conseils et de prendre des décisions de qualité qui vous aideront à progresser dans la vie.
 				</div>
 				<p class="p_quotes">
 					<a href="https://www.mensaotabil.com/products-page/ebooks/how-to-make-wise-decisions/"><img src="img/books/Howtomake.png" class="img_quote" style="float: left;"/></a>
@@ -79,6 +81,8 @@
 					<br/>
 					Receiving good advice is an art that can be perfected by practice. The quality of our lives is influenced by the people from whom we receive advice. For each one of us, there are people who enrich our lives and others who deplete our lives because of the quality of advice they give to us.
 					How to Make Wise Decisions presents as systematic and yet simple approach that involves six key steps to receiving good advice and making quality decisions which will help you advance in life.
+					<br/>
+					Recevoir de bons conseils est un art qui peut être perfectionné par la pratique. La qualité de notre vie est influencée par les gens de qui nous recevons des conseils. Pour chacun de nous, il ya des gens qui enrichissent nos vies et d'autres qui appauvrissent nos vies à cause de la qualité des conseils qu'ils donnent à nous.
 				</p>
 				<p class="p_quotes">
 					<!-- <img src="img/2_1.jpg" class="img_quote" style="float: left;"/> -->
